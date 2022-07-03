2e259c24aee29d448b331d167849e788866857bc
MateuszGorski1 <mateusz.gorski12345@wp.pl>
2022-07-03 11:21:41 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data";
+import React, { useState } from "react";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const removePerson = (id) => {
+    let newPeople = people.filter((p) => p.id !== id);
+    setPeople(newPeople);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container">
+      <h3> {people.length} birthdays today</h3>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+            <button onClick={() => removePerson(id)}>Remove me</button>
+          </article>
+        );
+      })}
+      <button
+        onClick={() => {
+          setPeople([]);
+        }}
+      >
+        Click to remove all birthdays
+      </button>
+    </section>
   );
 }
 
